a27d2ceb348bab9e68d117d7898ae6cd523f8028
Ruan Tiengo <ruan.rocha@contractor.rhimagnesita.com>
2022-12-16 01:23:58 -0300

    fix: remove s

--- components/Blog/Banner.tsx
+++ components/Blog/Banner.tsx
@@ -2,7 +2,7 @@ const Banner = () => {
     return (
       <div className='flex flex-col lg:flex-row lg:space-x-5 justify-between font-semibold px-10 py-5 mb-10'>
         <div>
-          <h1 className='text-5xl sm:text-7xl font-bold'>Ruan Tiengo's blog</h1>
+          <h1 className='text-5xl sm:text-7xl font-bold'>Ruan Tiengo{`&apos`}s blog</h1>
           <h2 className='mt-5 md:mt-1'>
             Witamy na{' '}
             <span className='underline decoration-2 decoration-accent'>
