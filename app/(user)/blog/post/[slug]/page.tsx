67c0eb1ed906b6886d41abb5b6ef36074ade8e28
Ruan Tiengo <ruan.rocha@contractor.rhimagnesita.com>
2022-12-17 18:24:32 -0300

    fix: description and title post

--- app/(user)/blog/post/[slug]/page.tsx
+++ app/(user)/blog/post/[slug]/page.tsx
@@ -43,63 +43,68 @@ const Post = async ({ params: { slug } }: Props) => {
   const post: Post = await client.fetch(query, { slug });
 
   return (
-    <article className='px-10 pb-28'>
-      <section className='space-y-2 border border-accent text-white'>
-        <div className='relative min-h-56 flex flex-col md:flex-row justify-between'>
-          <div className='absolute top-0 w-full h-full opacity-10 blur-sm p-10'>
-            <Image
-              className='object-cover object-center mx-auto'
-              src={urlFor(post.mainImage).url()}
-              alt={post.author.name}
-              fill
-            />
-          </div>
+    <>
+          <title>${post.title} | Ruan Tiengo - blog</title>
+        <meta name='description'>{post.description}</meta>
+    
+      <article className='px-10 pb-28'>
+        <section className='space-y-2 border border-accent text-white'>
+          <div className='relative min-h-56 flex flex-col md:flex-row justify-between'>
+            <div className='absolute top-0 w-full h-full opacity-10 blur-sm p-10'>
+              <Image
+                className='object-cover object-center mx-auto'
+                src={urlFor(post.mainImage).url()}
+                alt={post.author.name}
+                fill
+              />
+            </div>
 
-          <section className='p-5 bg-accent w-full'>
-            <div className='flex flex-col md:flex-row justify-between gap-y-5'>
-              <div>
-                <h1 className='text-4xl font-extrabold'>{post.title}</h1>
-                <p className=''>
-                  {new Date(post._createdAt).toLocaleDateString('pt-BR', {
-                    day: 'numeric',
-                    month: 'long',
-                    year: 'numeric',
-                  })}
-                </p>
-              </div>
-              <div className='flex items-center space-x-2'>
-                <Image
-                  className='rounded-full'
-                  src={urlFor(post.author.image).url()}
-                  alt={post.author.name}
-                  height={40}
-                  width={40}
-                />
+            <section className='p-5 bg-accent w-full'>
+              <div className='flex flex-col md:flex-row justify-between gap-y-5'>
+                <div>
+                  <h1 className='text-4xl font-extrabold'>{post.title}</h1>
+                  <p className=''>
+                    {new Date(post._createdAt).toLocaleDateString('pt-BR', {
+                      day: 'numeric',
+                      month: 'long',
+                      year: 'numeric',
+                    })}
+                  </p>
+                </div>
+                <div className='flex items-center space-x-2'>
+                  <Image
+                    className='rounded-full'
+                    src={urlFor(post.author.image).url()}
+                    alt={post.author.name}
+                    height={40}
+                    width={40}
+                  />
 
-                <div className='w-64'>
-                  <h3 className='text-lg font-bold'>{post.author.name}</h3>
-                  <div><PortableText value={post.author.bio} components={RichTextComponents} /></div>
+                  <div className='w-64'>
+                    <h3 className='text-lg font-bold'>{post.author.name}</h3>
+                    <div><PortableText value={post.author.bio} components={RichTextComponents} /></div>
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div>
-              <h2 className='italic pt-10'>{post.description}</h2>
-              <div className='flex items-center justify-end mt-auto space-x-2'>
-                  <p
-                    className='bg-gray-800 text-white px-3 py-1 rounded-full text-sm font-semibold mt-4'
-                  >
-                    {post.category.title}
-                  </p>
-              
+              <div>
+                <h2 className='italic pt-10'>{post.description}</h2>
+                <div className='flex items-center justify-end mt-auto space-x-2'>
+                    <p
+                      className='bg-gray-800 text-white px-3 py-1 rounded-full text-sm font-semibold mt-4'
+                    >
+                      {post.category.title}
+                    </p>
+                
+                </div>
               </div>
-            </div>
-          </section>
-        </div>
-      </section>
+            </section>
+          </div>
+        </section>
 
-      <PortableText value={post.body} components={RichTextComponents} />
-    </article>
+        <PortableText value={post.body} components={RichTextComponents} />
+      </article>
+    </>
   );
 };
 
