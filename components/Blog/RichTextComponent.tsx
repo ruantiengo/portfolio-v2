3656c692b4a3a912bb30c23894751ce85023b76d
Ruan Tiengo <ruan.rocha@contractor.rhimagnesita.com>
2022-12-17 17:39:25 -0300

    fix: page post

--- components/Blog/RichTextComponent.tsx
+++ components/Blog/RichTextComponent.tsx
@@ -47,14 +47,12 @@ export const RichTextComponents = {
   },
   marks: {
     link: ({ children, value }: any) => {
-      const rel = !value.href.startsWith('/')
-        ? 'noreferrer noopener'
-        : undefined;
+      
 
       return (
         <Link
-          href={value.href}
-          rel={rel}
+          href={"/" + value.href}
+          
           className='underline decoration-accent hover:decoration-black'
         >
           {children}
