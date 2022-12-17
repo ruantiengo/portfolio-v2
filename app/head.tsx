8be449befb4c498659fb97c5dbfd5ac0bd61f4b5
Ruan Tiengo <ruan.rocha@contractor.rhimagnesita.com>
2022-12-17 17:48:06 -0300

    fix: add meta

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,8 @@
 export default function Head() {
   return (
     <>
-      <title></title>
+      <title>Ruan Tiengo | Blog</title>
+      <meta name="description">Welcome to my blog here i will talk about react, node, nextjs, flutter, aspnet and css tricks.</meta>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
     </>
